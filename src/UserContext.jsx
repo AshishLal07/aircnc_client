d5f9076993c16b19d2079ebaf56e4eeb06d07a2a
AshishLal07 <74709197+AshishLal07@users.noreply.github.com>
2023-06-28 09:58:22 +0530

    upload aircnc_client side

---
+++ src/UserContext.jsx
@@ -0,0 +1,31 @@
+import axios from 'axios';
+import {createContext, useEffect, useState} from 'react';
+
+export const userContext = createContext({});
+
+// eslint-disable-next-line react/prop-types
+export function UserContextProvider({children}) {
+	const [user, setUser] = useState({});
+	const [token, setToken] = useState('');
+	const [render, setRender] = useState(false);
+
+	useEffect(() => {
+		if (Object.keys(user).length <= 0) {
+			axios
+				.get('/profile', {
+					headers: {Authorization: `Bearer ${localStorage.getItem('token')}`},
+				})
+				.then((data) => {
+					setUser(data.data);
+					setRender(true);
+				});
+		}
+		// eslint-disable-next-line react-hooks/exhaustive-deps
+	}, []);
+
+	return (
+		<userContext.Provider value={{user, setUser, render, setToken, token}}>
+			{children}
+		</userContext.Provider>
+	);
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: ["./src/**/*.{js,jsx,ts,tsx}", "./index.html"],
 
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        primary:"#f5385D",
+      }
+    },
   },
   plugins: [],
 }
